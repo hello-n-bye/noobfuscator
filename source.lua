8189387e5b58a6ad0e68dad0748be27da83e6199
suno <159689944+hello-n-bye@users.noreply.github.com>
2024-05-22 16:06:51 -0700

    Prompt used for v0.2

--- source.lua
+++ source.lua
@@ -1,7 +1,18 @@
 local content = "Hello, world from Noobfuscator example!"
 
+local exampleBoolean = true -- This will be changed to false
+
 local function foo(...)
    return print(...)
 end
 
+local function changeOperation(operation)
+   if (type(operation)) ~= "boolean" then
+      return error("Invalid operation type")
+   else
+      return operation == not operation
+   end
+end
+
+foo(changeOperation(exampleBoolean)); -- false
 foo(content); -- "Hello, world from Noobfuscator example!"
